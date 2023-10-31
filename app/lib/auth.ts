9a5ca9801dba97c03de582f2ab44f3b27c0cd8af
Haritha Hasathcharu <harithahasathcharu@icloud.com>
2023-10-31 17:38:07 +0530

    some changes

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -18,25 +18,12 @@ export const authOptions: NextAuthOptions = {
       profile(profile) {
         return {
           id: profile.sub,
-          name: profile.name,
+          name: profile.given_name + ' ' + profile.family_name,
           email: profile.username,
         };
       },
     },
   ],
-  callbacks: {
-    async session({ session, token, user }) {
-      session.idToken = token.idToken;
-      return session;
-    },
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.idToken = account.id_token;
-      }
-      return token;
-    },
-  },
   pages: {
     signIn: '/auth/signin',
   },
